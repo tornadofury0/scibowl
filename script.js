573babd3431891e3d433a52d186b3532e3ccff9d
tornadofury0 <131941323+tornadofury0@users.noreply.github.com>
2025-09-30 16:02:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // CONFIGURATION: Set your backend URL
-const API_BASE_URL = "http://104.154.189.254.ip.addr.tools:8080"; // Your Google Cloud VM backend
+const API_BASE_URL = "https://scibowl.myaddr.tools/health:8080"; // Your Google Cloud VM backend
 
 let typingJob = null;
 let currentQuestion = null;
